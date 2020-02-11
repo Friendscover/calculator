ce1e96459890d83884f9e40ad007712b4f8496fa
Friendscover <tobiaskloepper1@web.de>
2020-02-11 11:45:10 +0100

    Basic functionality finished

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,5 @@
-function operator(operator, x, y)
+//calc logic
+function decideOperator(operator, x, y)
 {
     if(operator == "*" || operator == "multiply")
     {
@@ -42,3 +43,58 @@ function divide (x, y)
     return (x / y);
 }
 
+let firstValue = "";
+let operation ="";
+
+//button event listener 
+const numberButtons = document.querySelectorAll(".numberContainer button");
+numberButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        fillInputField(button.id)
+    });
+});
+
+const calcButtons = document.querySelectorAll(".calcContainer button");
+calcButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        firstValue = parseInt(getInputFieldValue())
+        clearInput()
+        operation = button.id
+    });
+});
+
+const resultButton = document.querySelector(".equalContainer button");
+resultButton.addEventListener("click", () => {
+    updateTextInput(decideOperator(operation, firstValue, parseInt(getInputFieldValue())))
+});
+
+const clearButton = document.querySelector(".clearContainer button");
+clearButton.addEventListener("click" , () => {
+    clearInput()
+});
+
+//input population
+function fillInputField(id)
+{
+    let inputValue = getInputFieldValue();
+    if(id == 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+    {
+        inputValue = inputValue + id; 
+        updateTextInput(inputValue);
+    }
+}
+
+function updateTextInput(value)
+{
+    document.getElementById("result").value = value;
+}
+
+function clearInput()
+{
+    document.getElementById("result").value = "";
+}
+
+function getInputFieldValue()
+{
+    return document.getElementById("result").value;
+}
